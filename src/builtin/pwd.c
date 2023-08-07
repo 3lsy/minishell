e3ee199cc90725dab16b435b33e0a724a72f7701
Blackwood <107251262+Blackwoodd@users.noreply.github.com>
2023-08-07 12:47:47 +0200

    Create pwd.c

---
+++ src/builtin/pwd.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   a_pwd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbechon <nbechon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/19 15:32:40 by nbechon           #+#    #+#             */
+/*   Updated: 2023/07/12 15:07:27 by nbechon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+int     commande_pwd(char **word)
+{
+        char    cwd[1000];
+        int             i;
+
+        i = 0;
+        if (word[1] != NULL && word[1][0] == '-')
+        {
+                printf ("pwd: ");
+                while ((word[1][i] == '-' && i < 2) || i != 2)
+                {
+                        printf ("%c", word[1][i]);
+                        i++;
+                }
+                printf (": invalid option\n");
+                return (EXIT_FAILURE);
+        }
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+                printf("%s\n", cwd);
+                return (EXIT_SUCCESS);
+        }
+        else
+                return (perror("PWD"), EXIT_FAILURE);
+}
