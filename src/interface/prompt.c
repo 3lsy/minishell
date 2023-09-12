4d85c0ae8cc52a7438aeb0f8c9fbf340fcc245ce
3lsy <echavez-@student.42.fr>
2023-09-12 22:02:12 +0200

    norme

--- src/interface/prompt.c
+++ src/interface/prompt.c
@@ -6,25 +6,25 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 17:11:08 by echavez-          #+#    #+#             */
-/*   Updated: 2023/09/11 18:23:14 by echavez-         ###   ########.fr       */
+/*   Updated: 2023/09/12 22:01:44 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    init_prompt(t_prompt *prompt, char **ev)
+void	init_prompt(t_prompt *prompt, char **ev)
 {
-    prompt->username = ev[k24("USER")];
-    if (!prompt->username)
-        prompt->username = ev[k24("LOGNAME")];
-    prompt->hostname = ev[k24("HOSTNAME")]; 
-    prompt->pwd = ev[k24("PWD")];
-    if (!prompt->pwd)
-        prompt->pwd = getcwd(NULL, 0);
-    prompt->separator = ':';
-    if (!prompt->pwd)
-        prompt->separator = ' ';
-    prompt->symbol = '$';
-    if (ft_strcmp(prompt->username, "root") == 0)
-        prompt->symbol = '#';
+	prompt->username = ev[k24("USER")];
+	if (!prompt->username)
+		prompt->username = ev[k24("LOGNAME")];
+	prompt->hostname = ev[k24("HOSTNAME")];
+	prompt->pwd = ev[k24("PWD")];
+	if (!prompt->pwd)
+		prompt->pwd = getcwd(NULL, 0);
+	prompt->separator = ':';
+	if (!prompt->pwd)
+		prompt->separator = ' ';
+	prompt->symbol = '$';
+	if (ft_strcmp(prompt->username, "root") == 0)
+		prompt->symbol = '#';
 }
