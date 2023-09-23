a9b187add9141c4ca3be182a658438e60f746f90
3lsy <echavez-@student.42.fr>
2023-09-23 18:29:26 +0200

    echo builtin

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,8 +6,32 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 15:34:35 by echavez-          #+#    #+#             */
-/*   Updated: 2023/09/23 18:22:22 by echavez-         ###   ########.fr       */
+/*   Updated: 2023/09/23 18:29:07 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	ft_echo(int ac, char **av, __attribute__((unused)) char **ev)
+{
+	int	i;
+	int	n_flag;
+
+	i = 1;
+	n_flag = 0;
+	if (ac > 1 && !ft_strcmp(av[1], "-n"))
+	{
+		n_flag = 1;
+		i++;
+	}
+	while (i < ac)
+	{
+		ft_printf("%s", av[i]);
+		if (i + 1 < ac)
+			ft_printf(" ");
+		i++;
+	}
+	if (!n_flag)
+		ft_printf("\n");
+	return (EXIT_SUCCESS);
+}
