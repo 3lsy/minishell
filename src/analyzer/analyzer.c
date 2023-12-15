921b6f2edfb3218a3c892a4fc8ebe074fe35f6a8
3lsy <echavez-@student.42.fr>
2023-12-15 14:01:05 +0100

    exit status 2 on analyzer error

--- src/analyzer/analyzer.c
+++ src/analyzer/analyzer.c
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 15:04:10 by echavez-          #+#    #+#             */
-/*   Updated: 2023/12/15 13:59:07 by echavez-         ###   ########.fr       */
+/*   Updated: 2023/12/15 14:00:42 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,12 @@ int	ft_analyzer(t_sh *sh)
 {
 	sh->cl.tokens = ft_lexer(sh);
 	if (!sh->cl.tokens)
-	{
-		sh->cl.exit_status = 2;
 		return (EXIT_FAILURE);
-	}
 	if (ft_parser(sh->cl.tokens) == FALSE)
-	{
-		sh->cl.exit_status = 2;
 		return (EXIT_FAILURE);
-	}
 	sh->cl.cmds = ft_split_cmds(sh->cl.tokens);
 	if (!sh->cl.cmds)
-	{
-		sh->cl.exit_status = 2;
 		return (EXIT_FAILURE);
-	}
 	ft_syntax_tree(sh);
 	return (EXIT_SUCCESS);
 }
